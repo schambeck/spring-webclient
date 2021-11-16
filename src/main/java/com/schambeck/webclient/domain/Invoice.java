add02f45ca0b05c7b67dfd7602ce900e6e43dfc6
Marcelo Schambeck <mschambeck@gmail.com>
2021-11-15 22:34:09 -0300

    NoArgsConstructor removed

--- src/main/java/com/schambeck/webclient/domain/Invoice.java
+++ src/main/java/com/schambeck/webclient/domain/Invoice.java
@@ -2,14 +2,12 @@ package com.schambeck.webclient.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Invoice implements Serializable {
 
