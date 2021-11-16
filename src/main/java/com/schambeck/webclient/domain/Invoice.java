192a5b26ba361e4171fce56dd1336ad59ae06260
Marcelo Schambeck <mschambeck@gmail.com>
2021-11-16 03:00:42 -0300

    NoArgsConstructor

--- src/main/java/com/schambeck/webclient/domain/Invoice.java
+++ src/main/java/com/schambeck/webclient/domain/Invoice.java
@@ -2,15 +2,20 @@ package com.schambeck.webclient.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Invoice implements Serializable {
 
+    @EqualsAndHashCode.Include
     private Long id;
     private LocalDate issued;
     private BigDecimal total;
