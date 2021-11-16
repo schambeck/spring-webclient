8233c465f87b86291fd0400ddad3266c8bec761b
Marcelo Schambeck <mschambeck@gmail.com>
2021-11-15 22:12:39 -0300

    Revert delete

---
+++ src/main/java/com/schambeck/webclient/base/exception/ErrorData.java
@@ -0,0 +1,14 @@
+package com.schambeck.webclient.base.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorData {
+
+    private String message;
+
+}
